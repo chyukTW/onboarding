d127f41cbeaf91bcb1d91b65b5c272050a277ce9
chyukTW <ch22@twinny.co.kr>
2022-05-02 18:36:49 +0900

    Add useQuery

--- todolist/src/apollo/client.ts
+++ todolist/src/apollo/client.ts
@@ -1,6 +1,25 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import tasksVar from "./stores/tasks";
 
-const cache = new InMemoryCache({});
+// 방법 1. useReactiveVar 사용하는 경우
+// const cache = new InMemoryCache({});
+
+
+// 방법 2. useQuery 사용하는 경우
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        getTasks: {
+          read() {
+            return tasksVar();
+          },
+        },
+      },
+    },
+  },
+});
+
 
 const client = new ApolloClient({
   cache,
--- todolist/src/components/TaskList.tsx
+++ todolist/src/components/TaskList.tsx
@@ -1,15 +1,22 @@
-import { useReactiveVar } from "@apollo/client";
-import tasksVar from "../apollo/stores/tasks";
+import { useQuery, useReactiveVar } from "@apollo/client";
+import tasksVar, { Task } from "../apollo/stores/tasks";
+import { GET_TASKS } from "../graphql/tasks/query";
 
 import TaskItem from "./TaskItem";
 
 const TaskList = () => {
-  const tasks = useReactiveVar(tasksVar);
+  // 방법 1. useReactiveVar 사용하기
+  // const tasks = useReactiveVar(tasksVar);
+
+  // 방법 2. useQuery 사용하기,
+  // useQuery를 사용하면 query 및 InMemoryCache > typePolicies 작성해줘야 함
+  const { data } = useQuery(GET_TASKS);
+  const tasks = data.getTasks;
 
   return (
     <ul>
       {
-        tasks && tasks.map((task) => {
+        tasks && tasks.map((task: Task) => {
           return <TaskItem key={task.id} task={task} />
         })
       }
