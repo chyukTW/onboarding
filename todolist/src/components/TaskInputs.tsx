fe98a429e35386d5c94be80e1e2edb58ae6088f8
chyukTW <ch22@twinny.co.kr>
2022-05-02 17:24:40 +0900

    Add todo inputs

--- todolist/src/components/TaskInputs.tsx
+++ todolist/src/components/TaskInputs.tsx
@@ -1,8 +1,22 @@
+import React, { useState } from "react";
+
 const TaskInputs = () => {
+  const [todoValues, setTodoValues] = useState('');
+
+  const handleSubmitTodoForm = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(e.target);
+  }
+
+  const handleChangeTodoInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setTodoValues(e.target.value);
+  };
+
   return (
-    <div>
-      Input Form...
-    </div>
+    <form onSubmit={handleSubmitTodoForm}>
+      <input type="text" onChange={handleChangeTodoInput} value={todoValues} />
+      <button type="submit">submit</button>
+    </form>
   )
 };
 
