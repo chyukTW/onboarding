6f91df515f85c312be89ae926895d16c6e706a20
chyukTW <ch22@twinny.co.kr>
2022-05-03 16:06:48 +0900

    Add prisma

--- apollo-server/src/server.ts
+++ apollo-server/src/server.ts
@@ -1,16 +1,100 @@
 import { ApolloServerPluginLandingPageGraphQLPlayground as playGround } from "apollo-server-core";
 import { ApolloServer, gql } from 'apollo-server';
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const typeDefs = gql`
+  type Task {
+    id: Int!
+    text: String!
+    createdAt: String!
+    updatedAt: String!
+  }
+  type Result {
+    ok: Boolean!
+    error: String
+  }
   type Query {
-    hello: String
+    getTasks: [Task]
+    getTask(id: Int!): Task
+  }
+  type Mutation {
+    addTask(text: String): Task
+    deleteTask(id: Int): Result
+    updateTask(id: Int, text: String): Result
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    getTasks: () => client.task.findMany(),
+    getTask: (_: any, { id }: any) => {
+      return client.task.findUnique({where: {
+        id
+      }});
+    }
   },
+  Mutation: {
+    addTask: (_: any, { text }: any) => {
+      return client.task.create({
+        data: {
+          text
+        }
+      })
+    },
+    deleteTask: async (_: any, { id }: any) => {
+      try{
+        const task = await client.task.findUnique({where: {id}});
+  
+        if(!task){
+          return {
+            ok: false,
+            error: 'No Task'
+          }
+        }
+  
+        await client.task.delete({ where: {id}});
+  
+        return {
+          ok: true
+        }
+      }catch(e){
+        return {
+          ok: false,
+          error: e
+        }
+      }
+    },
+    updateTask: async (_: any, { id, text }: any) => {
+      try{
+        const task = await client.task.findUnique({where: {id}});
+
+        if(!task){
+          return {
+            ok: false,
+            error: 'No Task'
+          }
+        }
+
+        await client.task.update({ 
+          where: { id },
+          data: {
+            text
+          }
+        });
+
+        return {
+          ok: true
+        }
+      } catch(e) {
+        return {
+          ok: false,
+          error: e
+        }
+      }
+    },
+  }
 };
 
 const server = new ApolloServer({
